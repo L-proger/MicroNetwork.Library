b98e6185df55f1c665ccb38906ea9e73bbdec369
l-proger <l-proger@yandex.ru>
2023-01-07 05:10:28 +0300

    *

--- Interfaces/MicroNetwork.Task.MemoryAccess.h
+++ Interfaces/MicroNetwork.Task.MemoryAccess.h
@@ -55,7 +55,7 @@ namespace LFramework{
     struct InterfaceAbi<MicroNetwork::Task::MemoryAccess::IHostToDevice> : public InterfaceAbi<LFramework::IUnknown>{
         using Base = InterfaceAbi<LFramework::IUnknown>;
         //{cc3bcc31-5bba-4be5-b3bd-c7fd854a9c2d}
-        static constexpr InterfaceID ID() { return { 0xcc3bcc31, 0x4be55bba, 0xfdc7bdb3, 0x2d9c4a85  }; }
+        static constexpr InterfaceID ID() { return { 0xcc3bcc31, 0x4be55bba, 0xfdc7bdb3, 0x2d9c4a85 }; }
         virtual Result LFRAMEWORK_COM_CALL read(MicroNetwork::Task::MemoryAccess::MemoryRegion request) = 0;
         virtual Result LFRAMEWORK_COM_CALL write(MicroNetwork::Task::MemoryAccess::MemoryBlob request) = 0;
     private:
@@ -110,7 +110,7 @@ namespace LFramework{
     struct InterfaceAbi<MicroNetwork::Task::MemoryAccess::IDeviceToHost> : public InterfaceAbi<LFramework::IUnknown>{
         using Base = InterfaceAbi<LFramework::IUnknown>;
         //{da97a152-9e0c-43c1-bd60-571c8b4582df}
-        static constexpr InterfaceID ID() { return { 0xda97a152, 0x43c19e0c, 0x1c5760bd, 0xdf82458b  }; }
+        static constexpr InterfaceID ID() { return { 0xda97a152, 0x43c19e0c, 0x1c5760bd, 0xdf82458b }; }
         virtual Result LFRAMEWORK_COM_CALL readResponse(MicroNetwork::Task::MemoryAccess::MemoryBlob response) = 0;
         virtual Result LFRAMEWORK_COM_CALL writeResponse(bool success) = 0;
     private:
