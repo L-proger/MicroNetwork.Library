d9b9ce1af0688ddd4e99ac88d5a2fdbd164394be
L-proger <l-proger@yandex.ru>
2023-08-20 23:04:34 +0300

    *

--- Library.h
+++ Library.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <MicroNetwork.Host.h>
+#include <LFramework/COM/ComLibrary.h>
 
 namespace MicroNetwork::Host {
 
@@ -8,5 +9,7 @@ class Library : public LFramework::ComImplement<Library, LFramework::ComObject, 
 public:
     LFramework::ComPtr<MicroNetwork::Host::INetwork> createNetwork(std::uint16_t vid, std::uint16_t pid);
 };
+
+LFRAMEWORK_COM_LIBRARY_EXPORT_ENTRY_FUNCTION()
 
 }
